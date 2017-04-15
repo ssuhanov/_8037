52174d4097ba1cd616f0f8532916b970f6a22f50
Serge Sukhanov <ssuhanov@icloud.com>
2017-04-15 10:57:38 +0300

    #2 Init Objective-C project

---
+++ ObjectiveC8037/ObjectiveC8037/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveC8037
+//
+//  Created by Serge Sukhanov on 4/15/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
