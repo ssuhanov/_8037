11c33c51fbaacf25f7208196efe5bda7e9623b5f
Serge Sukhanov <ssuhanov@icloud.com>
2017-04-23 12:41:47 +0300

    #8 Add Game8037 class in CSharp

---
+++ CSharp8037/CSharp8037/Game8037.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp8037
+{
+	public class Game8037 : IGame
+	{
+		public Game8037()
+		{
+		}
+
+		public void greet(string exitWord)
+		{
+
+		}
+
+		public void checkUserAnswer(string userAnswer)
+		{
+
+		}
+	}
+}
