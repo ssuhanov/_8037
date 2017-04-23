b1f3d6b2c2287d3f50fd08773877b1fc4aab6026
Serge Sukhanov <ssuhanov@icloud.com>
2017-04-23 12:28:51 +0300

    #8 Add Game8037 class in Swift

---
+++ Swift8037/Swift8037/Game8037.swift
@@ -0,0 +1,23 @@
+//
+//  Game8037.swift
+//  Swift8037
+//
+//  Created by Serge Sukhanov on 4/23/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+import Foundation
+
+class Game8037: Game {
+    
+    // MARK: - Game
+    
+    func greet(with exitWord: String) {
+        
+    }
+    
+    func check(userAnswer: String) {
+        
+    }
+    
+}
