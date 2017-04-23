a9c36300d54b4ff6bfa7dafd9cee27bf6143487c
Serge Sukhanov <ssuhanov@icloud.com>
2017-04-23 12:31:27 +0300

    #8 Add Game8037 class in Objective-C

---
+++ ObjectiveC8037/ObjectiveC8037/Game8037.m
@@ -0,0 +1,21 @@
+//
+//  Game8037.m
+//  ObjectiveC8037
+//
+//  Created by Serge Sukhanov on 4/23/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+#import "Game8037.h"
+
+@implementation Game8037
+
+- (void)greetWithExitWord:(NSString *)exitWord {
+    
+}
+
+- (void)checkUserAnswer:(NSString *)userAnswer {
+    
+}
+
+@end
