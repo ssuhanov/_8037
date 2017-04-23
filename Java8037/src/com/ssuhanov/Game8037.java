9902aa780a5321a745f0ef49e49bfd71d209f686
Serge Sukhanov <ssuhanov@icloud.com>
2017-04-23 12:41:32 +0300

    #8 Add Game8037 class in Java

---
+++ Java8037/src/com/ssuhanov/Game8037.java
@@ -0,0 +1,17 @@
+package com.ssuhanov;
+
+/**
+ * Created by ssuhanov on 4/23/17.
+ */
+public class Game8037 implements Game {
+
+    @Override
+    public void greet(String exitWord) {
+
+    }
+
+    @Override
+    public void checkUserAnswer(String userAnswer) {
+
+    }
+}
